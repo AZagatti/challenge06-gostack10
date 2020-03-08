b4600e4141a1c1d2edde79b45e1292cfbe00d586
azagatti <andre.zagatti1@gmail.com>
2020-03-08 14:23:53 -0300

    feat: Create Main page

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User() {
+export default function User({ route, navigation }) {
+  console.tron.log(route.params);
+
   return <View />;
 }
